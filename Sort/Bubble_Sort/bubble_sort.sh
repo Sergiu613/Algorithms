375c6641aade0e13f3417109d79a1d00d010190f
Sergiu Olar <66427434+Sergiu613@users.noreply.github.com>
2026-03-17 09:21:53 +0200

    Create bubble_sort.sh

---
+++ Sort/Bubble_Sort/bubble_sort.sh
@@ -0,0 +1,15 @@
+# Data array
+data=(64 34 25 12 22 11 90)
+n=${#data[@]}
+
+for ((i = 0; i < n - 1; i++)); do
+    for ((j = 0; j < n - i - 1; j++)); do
+        if [ ${data[j]} -gt ${data[j+1]} ]; then
+            temp=${data[j]}
+            data[j]=${data[j+1]}
+            data[j+1]=$temp
+        fi
+    done
+done
+
+echo "Sorted array: ${data[@]}"
