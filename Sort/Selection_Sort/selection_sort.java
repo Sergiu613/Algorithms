d87cf089e31281e345fdd92193181ce71dc91003
Sergiu Olar <66427434+Sergiu613@users.noreply.github.com>
2026-03-18 14:43:02 +0200

    Create selection_sort.java

---
+++ Sort/Selection_Sort/selection_sort.java
@@ -0,0 +1,11 @@
+public static void selectionSort(int[] arr) {
+    for (int i = 0; i < arr.length - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < arr.length; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
