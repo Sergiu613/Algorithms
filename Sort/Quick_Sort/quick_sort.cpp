98c3f6a0bd111ddbb170b9aa7f4d7a61d32ade5d
Sergiu Olar <66427434+Sergiu613@users.noreply.github.com>
2026-05-06 11:49:25 +0300

    Create quick_sort.cpp

---
+++ Sort/Quick_Sort/quick_sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int partition(std::vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(std::vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    std::vector<int> data = {64, 34, 25, 12, 22, 11, 90};
+    quickSort(data, 0, data.size() - 1);
+    for (int x : data) std::cout << x << " ";
+    return 0;
+}
